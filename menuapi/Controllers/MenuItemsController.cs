43acc15178b3499149054e5874a1439bc569238f
dmytro vorobey <vorobeydo@gmail.com>
2020-03-04 21:32:58 +0200

    Set up an api

---
+++ menuapi/Controllers/MenuItemsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using MenuApi.Models.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MenuItemsController : ControllerBase
+    {
+        public IList<MenuItem> GetMenuItems()
+        {
+            return new List<MenuItem>()
+            {
+                new MenuItem()
+                {
+                    Id = Guid.NewGuid().ToString()
+                }
+            };
+        }
+    }
+}
---
+++ menuapi/Models/Responses/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuApi.Models.Responses
+{
+    public class MenuItem
+    {
+        public string Id { set; get; }
+    }
+}
